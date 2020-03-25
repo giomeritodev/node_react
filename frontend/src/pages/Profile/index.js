b303ba3da8e13610011e270f3b1d8fc201b299ee
Giomerito Alves de Souza <giomerito.dev@gmail.com>
2020-03-25 19:30:31 -0300

    Pegando nome do usuario do localStrorage

--- frontend/src/pages/Profile/index.js
+++ frontend/src/pages/Profile/index.js
@@ -6,11 +6,13 @@ import logo from './../../assets/logo.svg';
 import './style.css';
 
 export default function Profile(){
+	const ongName = localStorage.getItem('ongName');
+
 	return (
 		<div className="profile-container">
 			<header>
 				<img src={logo} alt="Be The Hero" />
-				<span>Bem vinda, APAE</span>
+				<span>Bem vinda, {ongName}</span>
 				
 				<Link className="button" to="/incidents/new">Cadastrar novo caso</Link>	
 				<button type="button">
