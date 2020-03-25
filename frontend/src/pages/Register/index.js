01821286661d39e66bdce96fb609cdef0efe91c9
Giomerito Alves de Souza <giomerito.dev@gmail.com>
2020-03-25 16:29:45 -0300

    CSS da pagina de REGISTER

--- frontend/src/pages/Register/index.js
+++ frontend/src/pages/Register/index.js
@@ -17,7 +17,7 @@ export default function Register(){
 
 					<Link className="back-link" to="/">
 						<FiArrowLeft size={16} color="#E02041" />
-						Não tenho cadastro
+						Efetuar LogIn
 					</Link>
 				</section>
 
