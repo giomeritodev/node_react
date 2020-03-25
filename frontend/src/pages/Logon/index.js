e7e00e0b78b763a708f608d11b400b22fbbb5330
Giomerito Alves de Souza <giomerito.dev@gmail.com>
2020-03-25 15:24:04 -0300

    CSS da pagina de login

--- frontend/src/pages/Logon/index.js
+++ frontend/src/pages/Logon/index.js
@@ -8,23 +8,27 @@ import logo from "./../../assets/logo.svg";
 export default function Logon(){
 	return (		
 		<div className="logon-container">
-			<section className="form">
-				<img src={logo} alt="Be The Hero" />
+		
+            <section className="form">
+			
+                <img src={logo} alt="Be The Hero" />
 				
 				<form>
 					<h1>Faça seu logOn</h1>
 
 					<input placeholder="Sua ID" />
-					<button type="submit">Entrar</button>
+					<button className="button" type="submit">Entrar</button>
 
 					<a href="/register">
 						<FiLogIn size={16} color="#E02041" />
 						Não tenho cadastro
 					</a>
 				</form>
+        
 			</section>
 
 			<img src={heroes} alt="Heroes" />
+        
 		</div>
 	);
 }
