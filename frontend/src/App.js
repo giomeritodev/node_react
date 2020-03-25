3c1b10f5c3bfe14bad5f0c239f905cda26d9e9e3
Giomerito Alves de Souza <giomerito.dev@gmail.com>
2020-03-25 12:38:42 -0300

    Conceito de estado dentro do react

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Header from './Header';
 
 function App() {
+  //Estado (retorna um array onde [valor atual, manipulador])
+  const [counter, setState] = useState(0);
+
+  function increment(){
+    setState(counter + 1);
+  }
+
   return (
-    <Header>
-      Giomerito Alves de Souza
-    </Header>
+    <div>
+      <Header>Contador: {counter}</Header>
+      <button onClick={increment}>Incrementar</button>
+    </div>
   );
 }
 
