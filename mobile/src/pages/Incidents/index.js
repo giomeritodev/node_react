c8643e3e438eb405654318b15215b9a6986222d0
Giomerito Alves de Souza <giomerito.dev@gmail.com>
2020-03-28 19:12:29 -0300

    Paginacao no mobile

--- mobile/src/pages/Incidents/index.js
+++ mobile/src/pages/Incidents/index.js
@@ -12,14 +12,30 @@ export default function Incidents(){
 	const [incidents, setIncidents] = useState([]);
 	const [total, setTotal] = useState(0);
 
+	const [page, setPage] = useState(1);
+	const [loading, setLoading] = useState(false);
+
 	function navigationToDetail(incidents){
 		navigation.navigate('Detail', { incidents });
 	}
 
 	async function loadIncidents(){
-		const response = await api.get('incidents');		
-		setIncidents(response.data.incidents);
-		setTotal(response.headers['x-total-count']);			
+		if(loading){
+			return;
+		}
+		if(total > 0 && incidents.length == total){
+			return;
+		}
+		setLoading(true);
+		
+		const response = await api.get('incidents', {
+			params: { page }
+		});		
+		setIncidents([...incidents, ...response.data.incidents]);
+		setTotal(response.headers['x-total-count']);
+
+		setPage(page + 1);
+		setLoading(false);			
 	}
 
 	useEffect(() => {
@@ -44,7 +60,11 @@ export default function Incidents(){
 				data={incidents}
 				style={styles.incidentList}
 				keyExtractor={incident => String(incident.id)}
-				showsVerticalScrollIndicator={false}
+				// showsVerticalScrollIndicator={false}
+				
+				onEndReached={loadIncidents}
+				onEndReachedThreshold={0.2}
+				
 				renderItem={({ item: incident }) => (
 					<View style={styles.incident}>
 						<Text style={[styles.incidentProperty, { marginTop: 0 }]}>ONG:</Text>
