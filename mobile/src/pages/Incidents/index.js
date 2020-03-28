7760b76296944cedf1f7940cb0204258c4af0d38
Giomerito Alves de Souza <giomerito.dev@gmail.com>
2020-03-27 23:29:11 -0300

    Colocando Scrool na pagina com FlatList

--- mobile/src/pages/Incidents/index.js
+++ mobile/src/pages/Incidents/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Feather } from '@expo/vector-icons';
-import { View, Image, Text, TouchableOpacity } from 'react-native';
+import { View, FlatList, Image, Text, TouchableOpacity } from 'react-native';
 
 import logoImg from './../../assets/logo.png';
 import styles from './style';
@@ -20,77 +20,27 @@ export default function Incidents(){
 			<Text style={styles.title}>Bem-vindo!</Text>
 			<Text style={styles.description}>Escolha um dos casos e salve o dia.</Text>
 
-			<View style={styles.incidentList}>
-				<View style={styles.incident}>
-					<Text style={styles.incidentProperty}>ONG:</Text>
-					<Text style={styles.incidentValue}>APAE</Text>
+			<FlatList 
+				data={[1, 2, 3, 4, 5]}
+				style={styles.incidentList}
+				renderItem={() => (
+					<View style={styles.incident}>
+						<Text style={styles.incidentProperty}>ONG:</Text>
+						<Text style={styles.incidentValue}>APAE</Text>
 
-					<Text style={styles.incidentProperty}>CASO::</Text>
-					<Text style={styles.incidentValue}>Cachorro atropelado</Text>
+						<Text style={styles.incidentProperty}>CASO::</Text>
+						<Text style={styles.incidentValue}>Cachorro atropelado</Text>
 
-					<Text style={styles.incidentProperty}>VALOR:</Text>
-					<Text style={styles.incidentValue}>R$ 120,00</Text>
+						<Text style={styles.incidentProperty}>VALOR:</Text>
+						<Text style={styles.incidentValue}>R$ 120,00</Text>
 
-					<TouchableOpacity style={styles.detailsButton} onPress={() => {}}>
-						<Text style={styles.detailsButtonText}>Ver mais detalhes</Text>
-						<Feather name="arrow-right" size={17} color="#E02041" />
-					</TouchableOpacity>	
-				</View>
-			</View>
-
-			<View style={styles.incidentList}>
-				<View style={styles.incident}>
-					<Text style={styles.incidentProperty}>ONG:</Text>
-					<Text style={styles.incidentValue}>APAE</Text>
-
-					<Text style={styles.incidentProperty}>CASO::</Text>
-					<Text style={styles.incidentValue}>Cachorro atropelado</Text>
-
-					<Text style={styles.incidentProperty}>VALOR:</Text>
-					<Text style={styles.incidentValue}>R$ 120,00</Text>
-
-					<TouchableOpacity style={styles.detailsButton} onPress={() => {}}>
-						<Text style={styles.detailsButtonText}>Ver mais detalhes</Text>
-						<Feather name="arrow-right" size={17} color="#E02041" />
-					</TouchableOpacity>	
-				</View>
-			</View>
-
-			<View style={styles.incidentList}>
-				<View style={styles.incident}>
-					<Text style={styles.incidentProperty}>ONG:</Text>
-					<Text style={styles.incidentValue}>APAE</Text>
-
-					<Text style={styles.incidentProperty}>CASO::</Text>
-					<Text style={styles.incidentValue}>Cachorro atropelado</Text>
-
-					<Text style={styles.incidentProperty}>VALOR:</Text>
-					<Text style={styles.incidentValue}>R$ 120,00</Text>
-
-					<TouchableOpacity style={styles.detailsButton} onPress={() => {}}>
-						<Text style={styles.detailsButtonText}>Ver mais detalhes</Text>
-						<Feather name="arrow-right" size={17} color="#E02041" />
-					</TouchableOpacity>	
-				</View>
-			</View>
-
-			<View style={styles.incidentList}>
-				<View style={styles.incident}>
-					<Text style={styles.incidentProperty}>ONG:</Text>
-					<Text style={styles.incidentValue}>APAE</Text>
-
-					<Text style={styles.incidentProperty}>CASO::</Text>
-					<Text style={styles.incidentValue}>Cachorro atropelado</Text>
-
-					<Text style={styles.incidentProperty}>VALOR:</Text>
-					<Text style={styles.incidentValue}>R$ 120,00</Text>
-
-					<TouchableOpacity style={styles.detailsButton} onPress={() => {}}>
-						<Text style={styles.detailsButtonText}>Ver mais detalhes</Text>
-						<Feather name="arrow-right" size={17} color="#E02041" />
-					</TouchableOpacity>	
-				</View>
-			</View>
+						<TouchableOpacity style={styles.detailsButton} onPress={() => {}}>
+							<Text style={styles.detailsButtonText}>Ver mais detalhes</Text>
+							<Feather name="arrow-right" size={17} color="#E02041" />
+						</TouchableOpacity>	
+					</View>
+				)}
+			/>		
 
 		</View>
 	);
